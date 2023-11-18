5efa25842102b2e460180ed6b1987e9cb42d83d5
DanielC <danchiappori@gmail.com>
2023-11-18 14:01:35 -0500

    Code modified to account for variable numbers of neurons. Note that dot product will accomplish this much more easily.

--- Neural_Network_Practice/neural_net_practice_ex1.cpp
+++ Neural_Network_Practice/neural_net_practice_ex1.cpp
@@ -1,21 +1,37 @@
 // BASICS ie. SINGLE NEURON
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <map>
 using namespace std;
 
 
 
-float neuron(vector<float> inputs, vector<float> weights, float bias) {
+vector<float> neuron(vector<float> inputs, map<int, vector<float>> weight_d, map<int, float> bias_d) {
 
-    // Find min of vectors so that cannot loop outside of bounds
-    int max_range =  min(inputs.size(), weights.size());
-
-    float sum = 0;
-    for (int i=0; i<max_range; i++) {
-        sum += inputs[i] * weights[i];
+    vector<float> neuron_output;
+    // Loop for each neuron ie. each key in weights
+    for (int i=0; i<weight_d.size(); i++) {
+        float sum = 0;
+        // Loop through input/weights vectors and tally the sum of their product
+        for (int j=0; j<inputs.size(); j++) {
+            sum += inputs[j] * weight_d[i][j];
+        }
+        neuron_output.push_back(sum + bias_d[i]);
     }
 
-    return sum + bias;
+    return neuron_output;
+}
+
+vector<float> random_weights(int input_size) {
+
+    vector<float> weight_vector;
+    weight_vector.resize(input_size);
+    for (int i=0; i<input_size; i++) {
+        weight_vector[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    }
+
+    return weight_vector;
 }
 
 
@@ -23,13 +39,22 @@ int main () {
 
     // Main input
     vector<float> inputs = {1.0, 2.0, 1.0, 2.5, 1.5, 1.0, 2.0}; // Adding 1 extra for practice
-    vector<float> weights = {0.4, 1.1, 0.3, 0.1, 0.9, 0.4};
-    float bias = 2.3;
+    int neuron_number = 5;
+
+    // Random weights for each neuron - assigned into hash table
+    map<int, vector<float>> weight_d = {};
+    map<int, float> bias_d = {};
+    for (int i=0; i<neuron_number; i++) {
+        weight_d[i] = random_weights(int(inputs.size()));
+        bias_d[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    }
 
     // Main function call to sum the product of input and weights
-    float sum = neuron(inputs, weights, bias);
+    vector<float> neuron_output = neuron(inputs, weight_d, bias_d);
 
-    cout << "Output: " << sum << endl;
+    for (float j : neuron_output) {
+        cout << j << endl;
+    }
 
     return 0;
 }
